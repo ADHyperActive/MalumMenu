e9a6d47db1b39c812e8103decaf3c5ca4e814be3
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2026-04-11 14:22:43 +0200

    fix: move check done on tab draw to menuui update
    Ensures "Open Config" can be toggled with keybinds even if Config tab is not selected

--- src/UI/Windows/Tabs/ConfigTab.cs
+++ src/UI/Windows/Tabs/ConfigTab.cs
@@ -19,12 +19,6 @@ public class ConfigTab : ITab
     {
         CheatToggles.openConfig = GUILayout.Toggle(CheatToggles.openConfig, " Open Config");
 
-        if (CheatToggles.openConfig)
-        {
-           Utils.OpenConfigFile();
-           CheatToggles.openConfig = false;
-        }
-
         CheatToggles.reloadConfig = GUILayout.Toggle(CheatToggles.reloadConfig, " Reload Config");
 
         CheatToggles.saveProfile = GUILayout.Toggle(CheatToggles.saveProfile, " Save to Profile");
